d3644f71a1a622624c89bdc9bb5f588b35ed79dd
Dustin Brett <dustin@voxter.ca>
2020-08-23 09:42:45 -0700

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "59b72c62837f245110fb";
+/******/ 	var hotCurrentHash = "0192ccedbd0253fc5334";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/uoOFH1K03NU2_bgt0aTwo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Ffb7d5399.8dd0f796be550efea9b7.js","static\u002Fcss\u002Fd0a0a60b1d686af880fe.css","static\u002Fchunks\u002Fpages\u002Findex-cdf20e472771e335cfe7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-83d9a4eab7fa36c1812c.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
