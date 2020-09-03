2dcd5edc0b2f6925069229f6c3f7f527f367c27b
Dustin Brett <dustin@voxter.ca>
2020-09-02 20:23:49 -0700

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eeb72bca3ba08ba4fc52";
+/******/ 	var hotCurrentHash = "fc434ecd8e3a9f90b8ad";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/pages/index.f6d406d33d878dcfca2c.hot-update.js
@@ -0,0 +1,58 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./assets/icons/CommanderKeen.png":
+/*!****************************************!*\
+  !*** ./assets/icons/CommanderKeen.png ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = \"/_next/static/images/CommanderKeen-a42c779cb0cd8ac777fc11189b647e86.png\";//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vYXNzZXRzL2ljb25zL0NvbW1hbmRlcktlZW4ucG5nP2Q4YTkiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEiLCJmaWxlIjoiLi9hc3NldHMvaWNvbnMvQ29tbWFuZGVyS2Vlbi5wbmcuanMiLCJzb3VyY2VzQ29udGVudCI6WyJtb2R1bGUuZXhwb3J0cyA9IFwiL19uZXh0L3N0YXRpYy9pbWFnZXMvQ29tbWFuZGVyS2Vlbi1hNDJjNzc5Y2IwY2Q4YWM3NzdmYzExMTg5YjY0N2U4Ni5wbmdcIjsiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./assets/icons/CommanderKeen.png\n");
+
+/***/ }),
+
+/***/ "./assets/icons/Doom.png":
+/*!*******************************!*\
+  !*** ./assets/icons/Doom.png ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = \"/_next/static/images/Doom-717e05e14ebca627eb93efac77dd116d.png\";//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vYXNzZXRzL2ljb25zL0Rvb20ucG5nP2NiOGMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEiLCJmaWxlIjoiLi9hc3NldHMvaWNvbnMvRG9vbS5wbmcuanMiLCJzb3VyY2VzQ29udGVudCI6WyJtb2R1bGUuZXhwb3J0cyA9IFwiL19uZXh0L3N0YXRpYy9pbWFnZXMvRG9vbS03MTdlMDVlMTRlYmNhNjI3ZWI5M2VmYWM3N2RkMTE2ZC5wbmdcIjsiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./assets/icons/Doom.png\n");
+
+/***/ }),
+
+/***/ "./assets/icons/Dos.png":
+/*!******************************!*\
+  !*** ./assets/icons/Dos.png ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = \"/_next/static/images/Dos-8cf0361d8e8ef3fc2a0976290d7ec003.png\";//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vYXNzZXRzL2ljb25zL0Rvcy5wbmc/MzgyZCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiIuL2Fzc2V0cy9pY29ucy9Eb3MucG5nLmpzIiwic291cmNlc0NvbnRlbnQiOlsibW9kdWxlLmV4cG9ydHMgPSBcIi9fbmV4dC9zdGF0aWMvaW1hZ2VzL0Rvcy04Y2YwMzYxZDhlOGVmM2ZjMmEwOTc2MjkwZDdlYzAwMy5wbmdcIjsiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./assets/icons/Dos.png\n");
+
+/***/ }),
+
+/***/ "./assets/icons/Files.png":
+/*!********************************!*\
+  !*** ./assets/icons/Files.png ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = \"/_next/static/images/Files-6f07e107c0b26f3da2a8b13e1df2d5c7.png\";//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vYXNzZXRzL2ljb25zL0ZpbGVzLnBuZz9lYzIwIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIiwiZmlsZSI6Ii4vYXNzZXRzL2ljb25zL0ZpbGVzLnBuZy5qcyIsInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0gXCIvX25leHQvc3RhdGljL2ltYWdlcy9GaWxlcy02ZjA3ZTEwN2MwYjI2ZjNkYTJhOGIxM2UxZGYyZDVjNy5wbmdcIjsiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./assets/icons/Files.png\n");
+
+/***/ }),
+
+/***/ "./assets/icons/Winamp.png":
+/*!*********************************!*\
+  !*** ./assets/icons/Winamp.png ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = \"/_next/static/images/Winamp-cceb8426c6768759fa142b6981e58f22.png\";//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vYXNzZXRzL2ljb25zL1dpbmFtcC5wbmc/M2E0NSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiIuL2Fzc2V0cy9pY29ucy9XaW5hbXAucG5nLmpzIiwic291cmNlc0NvbnRlbnQiOlsibW9kdWxlLmV4cG9ydHMgPSBcIi9fbmV4dC9zdGF0aWMvaW1hZ2VzL1dpbmFtcC1jY2ViODQyNmM2NzY4NzU5ZmExNDJiNjk4MWU1OGYyMi5wbmdcIjsiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./assets/icons/Winamp.png\n");
+
+/***/ })
+
+})
