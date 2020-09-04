97ed71b7bcc777e02a8d38405b37b03df31511c9
Dustin Brett <dustin@voxter.ca>
2020-09-04 16:02:50 -0700

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "757d5ae707f8ba9b977c";
+/******/ 	var hotCurrentHash = "297316c86bdc2a80f34e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
