a978f3b19de5e692bd04d63f2af058a821291463
Dustin Brett <dustin@voxter.ca>
2020-09-08 00:00:07 -0700

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e69df0f0edbb696c0775";
+/******/ 	var hotCurrentHash = "ea17e7ef1a7bd2cc190f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/pages/index.69848175dbf9cc1c25c0.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./public/index.json":
+/*!***************************!*\
+  !*** ./public/index.json ***!
+  \***************************/
+/*! exports provided: desktop, favicon.ico, fonts, games, icons, index.json, libs, mp3, pdf.worker.js, skins, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"desktop\\\":{\\\"Commander Keen.url\\\":null,\\\"DOS.url\\\":null,\\\"Doom.url\\\":null,\\\"Dustin Brett, Resume (2020).pdf\\\":null,\\\"Explorer.url\\\":null,\\\"Winamp.url\\\":null},\\\"favicon.ico\\\":null,\\\"fonts\\\":{\\\"segoeui.woff2\\\":null},\\\"games\\\":{\\\"doom.jsdos\\\":null,\\\"keen4.jsdos\\\":null},\\\"icons\\\":{\\\"apps\\\":{\\\"dos.png\\\":null,\\\"explorer.png\\\":null,\\\"winamp.png\\\":null},\\\"games\\\":{\\\"doom.png\\\":null,\\\"keen.png\\\":null},\\\"types\\\":{\\\"js.svg\\\":null,\\\"pdf.svg\\\":null,\\\"unknown.svg\\\":null}},\\\"index.json\\\":null,\\\"libs\\\":{\\\"vanta.waves.min.js\\\":null,\\\"wdosbox.js\\\":null,\\\"wdosbox.wasm.js\\\":null},\\\"mp3\\\":{\\\"demo.mp3\\\":null},\\\"pdf.worker.js\\\":null,\\\"skins\\\":{\\\"SpyAMP_Professional_Edition_v5.wsz\\\":null}}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3B1YmxpYy9pbmRleC5qc29uLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./public/index.json\n");
+
+/***/ })
+
+})
